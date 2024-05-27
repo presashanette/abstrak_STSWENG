ea327d2f01c88b0671475a842c0145ebe6f33ae6
Yuan <96109976+pngnyume@users.noreply.github.com>
2024-05-27 19:23:26 +0800

    Data from WIX

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,14 +1,14 @@
 const { Schema, model } = require('mongoose');
 
-const productSchema = new Schema ({
+const productSchema = new Schema({
     name: {type: String, required: true},
     picture: {type: String, required: true},
     price: {type: Number, required: true},
     SKU: {type: String, required: true},
     material: {type: [String], required: true},
     variations: {type: [Map], required: true},
-})
+});
 
-const Product = model('product', productSchema)
+const Product = model('product', productSchema);
 
 module.exports = Product;
