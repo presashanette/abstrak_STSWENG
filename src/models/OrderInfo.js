d240a12bd7c3ae6e2f23ca3ef940272c030bc871
Yuan <96109976+pngnyume@users.noreply.github.com>
2024-06-17 18:32:00 +0800

    orderinfos instead or orders in mongodb

--- src/models/OrderInfo.js
+++ src/models/OrderInfo.js
@@ -1,69 +1,41 @@
-// const mongoose = require('mongoose');
-
-// const OrderInfoSchema = new mongoose.Schema({
-//   orderNo: { type: String, required: true },
-//   date: { type: Date, required: true },
-//   totalOrderQuantity: { type: Number, required: true },
-//   items: [{ 
-//     name: String, 
-//     quantity: Number 
-//   }],
-//   paymentStatus: { type: String, required: true },
-//   paymentMethod: { type: String, required: true },
-//   fulfillmentStatus: { type: String, required: true },
-//   total: { type: Number, required: true }
-// });
-
-// module.exports = mongoose.model('OrderInfo', OrderInfoSchema);
-
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-// Item schema
-const itemSchema = new Schema({
-  itemName: String,
-  variant: String,
-  sku: String,
-  quantity: Number,
-  quantityRefunded: Number,
-  price: Number,
-  weight: Number,
-  customText: String,
-  depositAmount: Number,
-  deliveryTime: String,
-});
+const orderInfoSchema = new mongoose.Schema({
+    orderNumber: String,
+    dateCreated: Date,
+    time: String,
+    fulfillBy: String,
+    totalOrderQuantity: Number,
+    // contactEmail: String,
+    items: [{
+        itemName: String,
+        variant: String,
+        sku: String,
+        quantity: Number,
+        quantityRefunded: Number,
+        price: Number,
+        weight: Number,
+        customText: String,
+        depositAmount: Number,
+        deliveryTime: String
+    }],
+    paymentStatus: String,
+    paymentMethod: String,
+    couponCode: String,
+    giftCardAmount: Number,
+    shippingRate: Number,
+    totalTax: Number,
+    total: Number,
+    currency: String,
+    refundedAmount: Number,
+    netAmount: Number,
+    additionalFees: Number,
+    fulfillmentStatus: String,
+    trackingNumber: String,
+    fulfillmentService: String,
+    deliveryMethod: String,
+    shippingLabel: String,
+    orderedFrom: String
+}, { collection: 'orderInfos' });
 
-
-// Order schema
-const orderSchema = new Schema({
-  orderNumber: String,
-  dateCreated: Date,
-  time: String,
-  fulfillBy: String,
-  totalOrderQuantity: Number,
-  contactEmail: String,
-  noteFromCustomer: String,
-  additionalCheckoutInfo: String,
-  items: [itemSchema],
-  paymentStatus: String,
-  paymentMethod: String,
-  couponCode: String,
-  giftCardAmount: Number,
-  shippingRate: Number,
-  totalTax: Number,
-  total: Number,
-  currency: String,
-  refundedAmount: Number,
-  netAmount: Number,
-  additionalFees: Number,
-  fulfillmentStatus: String,
-  trackingNumber: String,
-  fulfillmentService: String,
-  deliveryMethod: String,
-  orderedFrom: String,
-  shippingLabel: String,
-});
-
-const Order = mongoose.model('Order', orderSchema);
-
-module.exports = Order;
+module.exports = mongoose.model('OrderInfo', orderInfoSchema);
