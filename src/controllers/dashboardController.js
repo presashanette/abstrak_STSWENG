f5be33aa6cc4d2bd02e05cbdf409d4facdaf9571
Victor <110458160+roirobertooo@users.noreply.github.com>
2024-11-21 13:19:36 +0800

    done with low inventory

--- src/controllers/dashboardController.js
+++ src/controllers/dashboardController.js
@@ -1,4 +1,5 @@
 const OrderInfo = require("../models/OrderInfo");
+const Product = require('../models/Product');
 
 async function getOrdersUnfulfilled(req, res) {
   try {
@@ -31,4 +32,35 @@ async function getOrdersCancelled(req, res) {
   }
 }
 
-module.exports = {getOrdersUnfulfilled, getOrdersFulfilled, getOrdersCancelled};
+async function getStocks(req, res) {
+  try {
+    const lowStockProducts = await Product.find({
+      variations: {
+        $elemMatch: {
+          stocks: {$lte: 5}
+        }
+      }
+    }).lean();
+
+    console.log(lowStockProducts);
+
+    const formattedProducts = lowStockProducts.flatMap(product =>
+        product.variations
+            .filter(variation => variation.stocks <= 5)
+            .map(variation => ({
+              sku: product.SKU,
+              name: `${product.name} (${variation.variation})`,
+              stocks: variation.stocks
+            }))
+    );
+
+    console.log(formattedProducts);
+
+    res.json(formattedProducts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Server Error');
+  }
+}
+
+module.exports = {getOrdersUnfulfilled, getOrdersFulfilled, getOrdersCancelled, getStocks};
